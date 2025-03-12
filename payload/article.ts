5ddfb7a762af3246f2e2021a67231ca9b768c8eb
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2025-03-12 15:29:41 +0900

    fix:favicon 수정

--- payload/article.ts
+++ payload/article.ts
@@ -5,13 +5,17 @@ const article: IArticle.Payload = {
 
   list: [
     {
-      content: 'Recipient of the "Open Source Hall of Fame" award in 2015.',
-      href: '#',
+      content: '(2023.12) 2023 NTIS 성과보고회 현장속으로!',
+      href: 'https://www.youtube.com/watch?v=-YzVHPDvi2E',
     },
     {
-      content: 'Featured on the cover of "Linux Monthly" as the face of Linux (2016, 2018, 2020).',
-      href: '#',
+      content: '(2023.07) 2023 환경데이터 활용 및 분석 공모전 "환경데이터 활용" 아이디어 기획 부문 대상 온리',
+      href: 'https://www.youtube.com/watch?v=FGyaAagh1GY',
     },
+    {
+      content: '(2023.06) 2023 환경데이터 공모전 시상…“탄소중립·자원순환 등 ESG테크 향연”',
+      href: 'https://v.daum.net/v/20230629151403309',
+    },    
   ],
 };
 
