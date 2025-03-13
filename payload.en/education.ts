f69ce1f7f8e745609bf73420298833b7368db1e5
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2025-03-13 12:27:01 +0900

    add:education 섹션 영문 작업 마무리

--- payload.en/education.ts
+++ payload.en/education.ts
@@ -5,8 +5,8 @@ const education: IEducation.Payload = {
 
   list: [
     {
-      title: '충남대학교',
-      subTitle: '컴퓨터융합학부 3학년 재학중 (GPA 3.89 / 4.5)',
+      title: 'Chungnam National University',
+      subTitle: 'Junior, Department of Computer Science and Engineering (GPA: 3.89 / 4.5)',
       startedAt: '2021-03',
       // endedAt: '2010-02',
     },
